f88e90c7b25bdcea1883583e9eaae6b218a78474
TIAGO COLADONATO <46878457@ort>
2023-07-04 10:39:29 -0300

    timeout done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import { useTimer } from 'react-timer-hook';
 
 function App() {
 
   const [listaPaises, setAllPaises] = useState([]);
   const [paisRandom, setPaisRandom] = useState({ flag: "https://img.tapimg.net/market/images/5f49418a4764b717ee0cee8bdd7c02fd.jpg?imageView2/0/w/720/h/405/q/80/format/jpg/interlace/1/ignore-error/1" });
   const [puntos, setPuntos] = useState(0)
+  
+  const [secondsLeft, setSecondsLeft] = useState(15);
+ 
 
 
   const SetRandomCountry = () => {
 
     let numRandom = Math.floor(Math.random() * 220);
-    
+
     setPaisRandom(listaPaises[numRandom])
     console.log(listaPaises[numRandom].name)
     return;
@@ -38,18 +42,22 @@ function App() {
     let aMostrar = document.querySelector('#gameUtilities')
     let aMostrar2 = document.getElementById('puntos')
     let aMostrar3 = document.getElementById('ayuda')
+    let inter = document.getElementById('intervalo')
 
     aMostrar.style.display = 'none'
     aMostrar2.style.display = 'none'
     aMostrar3.style.display = 'none'
-   
+    inter.style.display = 'none'
+
   }, [])
 
 
   function ComenzarJuego() {
 
-    SetRandomCountry()
+    setSecondsLeft(15)
 
+    SetRandomCountry()
+    let inter = document.getElementById('intervalo')
     let ocultar = document.querySelector('#startButton');
     let aMostrar = document.querySelector('#gameUtilities')
     let aMostrar2 = document.getElementById('puntos')
@@ -58,67 +66,96 @@ function App() {
     aMostrar.style.display = 'block'
     aMostrar2.style.display = 'block'
     aMostrar3.style.display = 'block'
-   
+    inter.style.display = 'block'
+
   }
 
 
 
   function CheckAnswer(e) {
-
+    console.log(e)
+ 
     e.preventDefault();
     let nombreInsertado = e.target.nombrePais.value
 
     if (nombreInsertado.toLowerCase() === (paisRandom.name).toLowerCase()) {
 
       SaltarMensaje(1)
-      setPuntos(puntos + 10)
+      setPuntos(puntos + 10 + secondsLeft)
+    
       SetRandomCountry()
 
     } else {
 
       SaltarMensaje(0)
-      SetRandomCountry()
       setPuntos(puntos - 1)
+      SetRandomCountry()
+
 
     }
+    e.target.nombrePais.value = ""
+    setSecondsLeft(15)
+
   }
 
 
 
-     const SaltarMensaje =  (numero) => {
-    
-     const mensaje = document.getElementById('mensaje');
-   
-      mensaje.textContent = numero == 0 ? 'INCORRECTO' : 'CORRECTO';
-      mensaje.style.backgroundColor = numero == 1 ? 'green' : 'red'
-      mensaje.style.display = 'block';
+  const SaltarMensaje = (numero) => {
 
-      let randomX = Math.floor(Math.random() * window.innerWidth);
-      let randomY = Math.floor(Math.random() * window.innerHeight);
-      mensaje.style.left = `${randomX}px`;
-      mensaje.style.top = `${randomY}px`;
+    const mensaje = document.getElementById('mensaje');
 
-      setTimeout(() => {
-        mensaje.style.display = 'none';
-      }, 2000);
+    mensaje.textContent = numero == 0 ? 'INCORRECTO' : 'CORRECTO';
+    mensaje.style.backgroundColor = numero == 1 ? 'green' : 'red'
+    mensaje.style.display = 'block';
 
-    
+    let randomX = Math.floor(Math.random() * window.innerWidth - 20);
+    let randomY = Math.floor(Math.random() * window.innerHeight - 20);
+    mensaje.style.left = `${randomX}px`;
+    mensaje.style.top = `${randomY}px`;
+
+    setTimeout(() => {
+      mensaje.style.display = 'none';
+    }, 2000);
+
+
 
   }
 
-    const PedirAyuda = () =>{
+  const PedirAyuda = () => {
 
-    let contAyuda = document.getElementById('ayuda1');
-    contAyuda.append("el pais empieza con: " +  paisRandom.name[0]);
+  }
+
+  const BajarSegundos = () => {
+    setSecondsLeft(secondsLeft - 1);
+    
+  }
+
+  useEffect(() => {
+
+    if (secondsLeft > 0) {
+      
+      const interval = setInterval(() => {
+     
+         BajarSegundos()
+
+      }, 1000);
+
+      return () => clearInterval(interval);
 
 
-
+    }else{
+      //CheckAnswer()
     }
+
+      
+        
+    });
 
   return (
     <>
 
       <div className='container'>
+      <p style={{color:'white', fontSize:'25px'}} id='intervalo'>Tiempo restante: {secondsLeft}</p>
         <img src={paisRandom.flag} alt='flag' className='flagImg'></img>
 
         <button onClick={() => ComenzarJuego()} className='startButton' id='startButton'>Empezar</button>
@@ -137,9 +174,10 @@ function App() {
         <div id="mensaje" className="mensaje"></div>
         <button type='submit' id='ayuda' className='button' onClick={() => PedirAyuda()}>Ayuda</button>
 
-        
-    
-  
+           
+         
+
+
 
       </div>
     </>
